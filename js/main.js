610d774c9331197adb78b0d556f2dcb14477c535
tommyHellraiser <nacho.ponce25@gmail.com>
2023-12-03 04:28:34 -0300

    Fixes and improvements to responsiveness

--- js/main.js
+++ js/main.js
@@ -11,7 +11,13 @@ $(document).ready(function($) {
     // });
     if ($(window).width() < 500) {
         $('#logo_image').addClass('left_aligned').removeClass('middle_aligned');
+        $('#location_footer').addClass('middle_aligned').removeClass('left_aligned');
+        $('.contact_info').addClass('hidden');
+        $('#contact_section_footer').removeClass('right_aligned_footer');
     } else {
         $('#logo_image').removeClass('left_aligned').addClass('middle_aligned');
+        $('#location_footer').removeClass('middle_aligned').addClass('left_aligned');
+        $('.contact_info').removeClass('hidden');
+        $('#contact_section_footer').addClass('right_aligned_footer');
     }
 });
