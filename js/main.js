c968c288efc1fd329212e02c3d3f45a538bbefd0
tommyHellraiser <nacho.ponce25@gmail.com>
2023-12-03 03:36:53 -0300

    Improved some stuff, still with commented code. Massive wip

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-$("#asd").text("Olah que onda");
+// Responsive design elements
+
+$(document).ready(function($) {
+    // $(window).on("resize", function() {
+    //    if ($(window).width() < 500) {
+    //        console.log("Mobile!");
+    //        $('#logo_image').addClass('left_aligned').removeClass('middle_aligned');
+    //    } else {
+    //        $('#logo_image').removeClass('left_aligned').addClass('middle_aligned');
+    //    }
+    // });
+    if ($(window).width() < 500) {
+        $('#logo_image').addClass('left_aligned').removeClass('middle_aligned');
+    } else {
+        $('#logo_image').removeClass('left_aligned').addClass('middle_aligned');
+    }
+});
